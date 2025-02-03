e552fb3716917605ba7ba3a804481fa0e9bd1717
NovemberJr <jr.november@gmail.com>
2025-02-03 18:40:30 +0700

    Added product group and product card

---
+++ components/shared/products-group-list.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import React, { useEffect, useInsertionEffect, useRef } from "react";
+import { useIntersection } from 'react-use';
+import { ProductCard, Title } from ".";
+
+interface Props {
+    title: string;
+    items: any[];
+    categoryId: number;
+    className?: string;
+    listClassName?: string;
+}
+
+export const ProductsGroupList: React.FC<Props> = ({ title, items, categoryId, className, listClassName }) => {
+    const intersectionRef = useRef(null);
+    const intersection = useIntersection(intersectionRef, { threshold: 0.4 });
+
+    useEffect(() => {
+        if (intersection?.isIntersecting) {
+            console.log(title, categoryId)
+        }
+    }, [intersection]);
+
+    return <div className={className} id={title} ref={intersectionRef}>
+        <Title text={title} size="lg" className="font-extrabold mb-5" />
+
+        <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
+            {items.map((product, i) => (
+                <ProductCard key={product.id} id={product.id} name={product.name} imageUrl={product.imageUrl} price={product.price} />
+            ))}
+        </div>
+    </div>
+}
